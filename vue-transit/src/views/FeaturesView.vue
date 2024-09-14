3872cb7281f82f83393f603a4cbcf2911c33311f
Beinrain06 <elngoueni@gmail.com>
2024-09-14 08:23:19 -0400

    features page --update-modal page v1

--- vue-transit/src/views/FeaturesView.vue
+++ vue-transit/src/views/FeaturesView.vue
@@ -1,55 +1,159 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive, computed } from 'vue'
+import router from '../router/index.ts'
+
+type ModalType = {
+  content_1: string
+  content_2: string
+}
+
+const modal = reactive<ModalType>({
+  content_1: '',
+  content_2: ''
+})
+
+const modal_toggle: boolean = ref(false)
+
+const modFeatures = ref()
+
+function selectedFeature(label: string) {
+  modFeatures.value.classList.add('modal_active')
+
+  switch (label) {
+    case 'request':
+      modal.content_1 = 'INTERNAL OPERATION'
+      modal.content_2 = 'EXTERNAL OPERATION'
+      break
+    case 'feedback':
+      modal.content_1 = 'SEND A FEEDBACK'
+      modal.content_2 = 'ASK FOR FEEDBACK'
+      break
+    case 'stat':
+      modal.content_1 = 'DIVISION STATS'
+      modal.content_2 = 'INDIVIDUAL STATS'
+      break
+    default:
+      throw Error('something went wrong in selectedFeature')
+  }
+}
+
+function routingModal(e, id: number, label: string) {
+  console.log(e.target)
+  if (id === 1) {
+    // router push
+  } else {
+    // router push
+  }
+}
+
+function closeModal() {
+  setTimeout(() => {
+    modal.content_1 = ''
+    modal.content_2 = ''
+  }, 1500)
+  modFeatures.value.classList.remove('modal_active')
+}
+</script>
 <template>
   <main id="features_page">
     <div class="gen_features_wrap">
+      <div class="back_features_link w-full h-4 py-1">
+        <span class="text-xl relative cursor-pointer"> &#8249; &#8249; &#8592;</span
+        ><span class="mx-2">back</span>
+      </div>
       <div class="signing_logo">
         <h3 class="text-sm mt-2 py-2 px-1 krub-regular-italic">transi\com</h3>
       </div>
-      <div class="features_com_wrap p-2">
+      <div class="features_com_wrap">
         <div class="features_com_content text-white">
-          <div id="request_ft" class="feature_com">
+          <div
+            id="request_ft"
+            class="feature_com"
+            @click.prevent="() => selectedFeature('request')"
+          >
             <h3 class="featute_title">REQUESTS</h3>
           </div>
-          <div id="feedback_ft" class="feature_com">
+          <div
+            id="feedback_ft"
+            class="feature_com"
+            @click.prevent="() => selectedFeature('feedback')"
+          >
             <h3 class="featute_title">FEEDBACKS</h3>
           </div>
-          <div id="stat_ft" class="feature_com">
+          <div id="stat_ft" class="feature_com" @click.prevent="() => selectedFeature('stat')">
             <h3 class="featute_title">STATS</h3>
           </div>
         </div>
       </div>
       <div class="lan_wrapper">
-        <select class="w-20 h-7 text-sm pl-2">
+        <select class="w-20 h-7 pl-2">
           <option value="english">english</option>
           <option value="french">french</option>
         </select>
+      </div>
+
+      <div class="modal_features" ref="modFeatures">
+        <div class="modal_features_container">
+          <div class="btn_modal_wrapper">
+            <button class="btn_close_modal" @click.prevent="closeModal">&#x2715;</button>
+          </div>
+          <div class="modal_features_content">
+            <div
+              id="modal_base_1"
+              class="modal_base flex_row_center"
+              @click.prevent="(e) => routingModal(e, 1, modal.content_1)"
+            >
+              <h4 class="modal_title text-center">{{ modal.content_1 }}</h4>
+            </div>
+            <div
+              id="modal_base_2"
+              class="modal_base flex_row_center"
+              @click.prevent="(e) => routingModal(e, 2, modal.content_2)"
+            >
+              <h4 class="modal_title text-center">{{ modal.content_2 }}</h4>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
 </template>
 <style scoped>
 @media (min-width: 180px) {
+  .flex_row_center {
+    @apply flex justify-center items-center;
+  }
+
   .gen_features_wrap {
     position: relative;
     width: 100vw;
     height: 100vh;
-    padding: 1rem;
+    padding: 0.5rem 1rem;
+    z-index: 1;
   }
 
   .signing_logo {
     position: absolute;
-    bottom: 0;
+    bottom: 0rem;
     right: 1rem;
   }
 
+  .lan_wrapper {
+    position: absolute;
+    top: 3.25rem;
+    font-size: calc(11px + 0.1vw);
+  }
+
   .features_com_wrap {
+    position: relative;
     width: 100%;
-    height: 100%;
+    height: 92%;
   }
 
   .features_com_content {
     width: 90%;
-    margin: 1rem auto 0;
+    margin: 1rem auto;
+    padding: 1rem 0;
     position: relative;
     top: 50%;
     left: 0;
@@ -61,14 +165,20 @@
 
   .feature_com {
     margin: 2.25rem 0;
-    width: 80vw;
+    width: 100%;
     height: 3rem;
+    cursor: pointer;
     border-radius: 5px;
     outline: 3px solid rgb(92, 92, 92);
     transition: all 1s ease-in-out;
+    transform: scale(1);
     display: grid;
     place-items: center;
     @apply grid place-items-center bg-gray-400;
+  }
+
+  .feature_com:focus {
+    transform: scale(0.98);
   }
 
   .feature_com:hover {
@@ -76,9 +186,116 @@
     @apply bg-green-300;
   }
 
-  .lan_wrapper {
+  /** modal features **/
+
+  .modal_features {
     position: absolute;
-    top: 1rem;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    visibility: hidden;
+    z-index: -1;
+  }
+
+  .modal_features.modal_active {
+    perspective: 1000px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    visibility: visible;
+    z-index: 3;
+  }
+
+  .modal_features .modal_features_container {
+    position: relative;
+    top: 50%;
+    left: 50%;
+    width: 20%;
+    height: 20%;
+    background-color: rgba(0, 0, 0, 0.35);
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    visibility: hidden;
+    z-index: 0;
+    transition: all 820ms ease-in-out 500ms;
+  }
+
+  .modal_features.modal_active .modal_features_container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(40, 134, 106, 0.65);
+    border-radius: 0;
+    visibility: visible;
+    z-index: 3;
+    transition: all 820ms ease-in-out;
+  }
+
+  .btn_modal_wrapper {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    padding: 1rem;
+    height: 3rem;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    z-index: 5;
+  }
+
+  .btn_close_modal {
+    cursor: pointer;
+    font-size: calc(22px + 0.25vw);
+    padding: 0 0.5rem;
+    border-radius: 5px;
+    transition: all 1s ease;
+  }
+
+  .btn_close_modal:hover {
+    @apply bg-gray-800;
+  }
+
+  .modal_features_content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 100%;
+    height: 100%;
+    padding: 1rem 2rem;
+    transform: translate(-50%, -50%);
+    @apply flex flex-col justify-center items-center;
+  }
+
+  .modal_base {
+    position: relative;
+    margin: 1rem 0;
+    border-radius: 10px;
+    cursor: pointer;
+    z-index: 10;
+    outline: 2px solid rgba(114, 134, 40, 0.89);
+    transition: all 1s ease;
+    @apply w-4/5 h-24 bg-gray-300 hover:bg-green-500;
+  }
+
+  .modal_features .modal_title {
+    position: relative;
+    top: 0.5rem;
+    transform: scale(0.3);
+    visibility: hidden;
+    opacity: 0.3;
+    transition: all 450ms ease-in-out;
+  }
+
+  .modal_features.modal_active .modal_title {
+    position: relative;
+    top: 0;
+    transform: scale(1);
+    visibility: visible;
+    opacity: 1;
+    transition: all 600ms ease-in-out;
   }
 }
 
@@ -86,7 +303,7 @@
   .signing_logo {
     position: absolute;
     bottom: unset;
-    top: 1rem;
+    top: 2.5rem;
     left: 1rem;
     right: 1rem;
   }
@@ -131,6 +348,38 @@
     top: unset;
     bottom: 1rem;
     right: 1rem;
+  }
+}
+
+@media (min-width: 300px) and (max-width: 960px) and (orientation: landscape) {
+  .gen_features_wrap {
+    min-height: 190vh;
+  }
+}
+
+@keyframes anim-modal-in {
+  0% {
+    opacity: 0;
+    transform: scale(0.45);
+    background-color: rgba(0, 0, 0, 0.35);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+    background-color: rgba(40, 134, 106, 0.75);
+  }
+}
+
+@keyframes anim-modal-out {
+  0% {
+    opacity: 1;
+    transform: scale(1);
+    background-color: rgba(40, 134, 106, 0.35);
+  }
+  100% {
+    opacity: 0;
+    transform: scale(0.35);
+    background-color: rgba(0, 0, 0, 0.35);
   }
 }
 </style>
